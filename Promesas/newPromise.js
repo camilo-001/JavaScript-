a692228f3834a70b29e78d3a56476e4ef06378d7
camilo-001 <43528989+camilo-001@users.noreply.github.com>
2021-02-17 09:51:24 -0500

    Update newPromise.js

--- Promesas/newPromise.js
+++ Promesas/newPromise.js
@@ -1,4 +1,4 @@
-// MANEJANDO PROMESAS DE MANERA ASINCRONA/home/milles/Documentos/Cursos/appAlmuersi
+// MANEJANDO PROMESAS DE MANERA ASINCRONA
 
 new Promise((resolve, reject) =>{
     setTimeout(() => resolve(2), 1000)
